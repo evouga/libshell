5a5ec316ce13e14b5fa7e9397d6c8758dfee3581
Kevin Song <chips@ksong.dev>
2023-10-23 15:49:10 -0500

    Working(?) Enzyme build.

--- src/MeshConnectivity.cpp
+++ src/MeshConnectivity.cpp
@@ -3,6 +3,16 @@
 #include <vector>
 #include <map>
 
+
+extern double __enzyme_autodiff(void*, double);
+double square(double x) {
+    return x * x;
+}
+double dsquare(double x) {
+    // This returns the derivative of square or 2 * x
+    return __enzyme_autodiff((void*) square, x);
+}
+
 namespace LibShell {
 
     MeshConnectivity::MeshConnectivity()
